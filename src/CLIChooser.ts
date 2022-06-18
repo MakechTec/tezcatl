73fd9ebaf3bd2ac31d05a620f2aedff131e01a62
Angel Dominguez <angeldomp49@gmail.com>
2022-06-18 14:53:15 -0500

    config

--- src/CLIChooser.ts
+++ src/CLIChooser.ts
@@ -1,20 +1,45 @@
-import {CLI} from "@makechtec/tezcatl-cli";
+import {CLI, Reader} from "@makechtec/tezcatl-cli";
+import {cwd} from "node:process";
+import {CONFIG_FILE} from "@makechtec/tezcatl-constants";
 
 export class Chooser{
  
     find(){
-        let preset = CLI.getArgumentValue("preset");
 
-        if(preset.value === ""){
-            preset.value = "default";
+        let config = this.readConfig();
+
+        let presetName = CLI.getArgumentValue("preset");
+
+        if(presetName === ""){
+            presetName = config.preset;
+
+            if(presetName === ""){
+                presetName = "default";
+            }
         }
 
-        let presetLocation = PRESET_DIR_PREFIX + preset.value;
+        let presetLocation = PRESET_DIR_PREFIX + presetName;
 
         return presetLocation;
     }
+
+    readConfig(){
+        let configFile = cwd() + "/" + CONFIG_FILE;
+        let content = Reader.read(configFile);
+
+        try{
+            return JSON.parse(content);
+        }
+        catch(e){
+            console.error(e);
+            return {
+                preset: ""
+            };
+        }
+
+    }
 };
 
-const PRESET_DIR_PREFIX = "@makechtec/tezcatl-preset-";
+export const PRESET_DIR_PREFIX = "@makechtec/tezcatl-preset-";
 
 export const CLIChooser = new Chooser();
