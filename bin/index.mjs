d3af8f2e15cd5f615e10fa0385c971d5bc194566
Angel Dominguez <angeldomp49@gmail.com>
2022-06-24 00:16:36 -0500

    presets.registry.mjs

--- bin/index.mjs
+++ bin/index.mjs
@@ -1,7 +1,7 @@
 #! /usr/bin/env node
 
 import {CLIChooser} from "../index.js";
-import presets from "../presets.registry.js";
+import presets from "../presets.registry.mjs";
 
 const p = CLIChooser.find();
 
